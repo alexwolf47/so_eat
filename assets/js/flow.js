41bef485142853fa57392cd6884c9215dfa4bb48
alexwolf47 <alexwolf47@gmail.com>
2021-07-19 22:04:44 +0100

    refactor: frontend input validation

--- assets/js/flow.js
+++ assets/js/flow.js
@@ -1,206 +1,254 @@
 const axios = require('axios').default;
 
-
 window.addEventListener('load', () => {
-    cta_button = document.querySelector(".js-cta")
-    submit_button = document.querySelector(".js-form-submit")
+    ctaButton = document.querySelector(".js-cta")
+    submitButton = document.querySelector(".js-form-submit")
+    productPageElement = document.querySelector(".js-product-page")
+    addressPage = document.querySelector(".js-address-page")
+    completionPage = document.querySelector(".js-complete-page")
 
-    product_page = document.querySelector(".js-product-page")
-    address_page = document.querySelector(".js-address-page")
-    completion_page = document.querySelector(".js-complete-page")
-
-    const selected_partition = document.querySelector(".js-selected-partition")
-
-
-    cta_button.addEventListener("click", () => {
+    ctaButton.addEventListener("click", () => {
         axios.get("/api/products")
             .then(function (response) {
-                cta_button.classList.add("is-hidden")
-                product_page.classList.add("is-shown")
-                response.data.map(product => {
-                    const product_div = document.createElement("div")
-                    product_div.classList.add("c-hero-product")
-                    const product_title = document.createElement("p")
-                    const product_title_text = document.createTextNode(product.name)
-                    product_title.appendChild(product_title_text)
-                    product_div.appendChild(product_title)
-                    const sku_container = document.createElement("div")
-                    sku_container.classList.add("c-hero-product-button-container")
-
-                    product.skus.map(sku => {
-                        const sku_div = document.createElement("div")
-                        sku_div.classList.add("c-hero-product-button")
-                        const sku_title = document.createTextNode(sku.name)
-                        sku_div.appendChild(sku_title)
-                        if (!sku.has_stock) {
-                            sku_div.classList.add("out-of-stock")
-                        }
-                        if (sku.has_stock) {
-                            sku_div.addEventListener("click", () => {
-                                selected_partition.value = sku.id
-                                product_page.classList.remove("is-shown")
-                                address_page.classList.add("is-shown")
-                            }
-                            )
-                        }
-
-                        sku_container.appendChild(sku_div)
-
-                    })
-                    product_div.appendChild(sku_container)
-                    const out_of_stock_tooltip = document.createElement("p")
-                    const out_of_stock_tooltip_text = document.createTextNode("* red = out of stock")
-                    out_of_stock_tooltip.appendChild(out_of_stock_tooltip_text)
-                    product_div.appendChild(out_of_stock_tooltip)
-                    product_page.appendChild(product_div)
-                }
-                )
+                ctaButton.classList.add("is-hidden")
+                constructProductPage(productPageElement, response)
 
             })
             .catch(function (error) {
                 // handle error
-                cta_button.innerHTML = "Something went wrong!"
-                cta_button.value = "Something went wrong!"
-                cta_button.classList.add("is-invalid")
+                ctaButton.innerHTML = "Something went wrong!"
+                ctaButton.value = "Something went wrong!"
+                ctaButton.classList.add("is-invalid")
                 setTimeout(function () {
-                    cta_button = document.querySelector(".js-form-submit")
-                    cta_button.classList.remove("is-invalid")
+                    ctaButton = document.querySelector(".js-form-submit")
+                    ctaButton.classList.remove("is-invalid")
                 }, 500)
                 return
             })
     })
 
-    submit_button.addEventListener("click", () => {
-        submit_button.classList.remove("is-invalid")
-        selected_partition_input = document.querySelector(".js-selected-partition")
-        name_input = document.querySelector(".js-name-input")
-        email_input = document.querySelector(".js-email-input")
-        line1_input = document.querySelector(".js-line1-input")
-        postcode_input = document.querySelector(".js-postcode-input")
-        town_input = document.querySelector(".js-town-input")
-        county_input = document.querySelector(".js-county-input")
+    addInputBlurValidation()
 
-        if (name_input.value == "") {
-            name_input.classList.add("is-invalid")
+    submitButton.addEventListener("click", () => {
+        submitButton.classList.remove("is-invalid")
+        submitOrder()
+    }
+    )
+})
 
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
+function constructProductPage(productPageElement, response) {
+    const selectedPartition = document.querySelector(".js-selected-partition")
 
+    productPageElement.classList.add("is-shown")
+    response.data.map(product => {
+        const productDiv = document.createElement("div")
+        productDiv.classList.add("c-hero-product")
+        const productTitle = document.createElement("p")
+        const productTitleText = document.createTextNode(product.name)
+        productTitle.appendChild(productTitleText)
+        productDiv.appendChild(productTitle)
+        const skuContainer = document.createElement("div")
+        skuContainer.classList.add("c-hero-product-button-container")
+
+        product.skus.map(sku => {
+            const skuDiv = document.createElement("div")
+            skuDiv.classList.add("c-hero-product-button")
+            const skuTitle = document.createTextNode(sku.name)
+            skuDiv.appendChild(skuTitle)
+            if (!sku.has_stock) {
+                skuDiv.classList.add("out-of-stock")
+            } else {
+                skuDiv.addEventListener("click", () => {
+                    selectedPartition.value = sku.id
+                    productPageElement.classList.remove("is-shown")
+                    addressPage.classList.add("is-shown")
+                })
+            }
+            skuContainer.appendChild(skuDiv)
+        })
+        productDiv.appendChild(skuContainer)
+        const outOfStockTooltip = document.createElement("p")
+        const outOfStockTooltipText = document.createTextNode("* red = out of stock")
+        outOfStockTooltip.appendChild(outOfStockTooltipText)
+        productDiv.appendChild(outOfStockTooltip)
+        productPageElement.appendChild(productDiv)
+    }
+    )
+}
+
+function addInputBlurValidation() {
+    nameInput = document.querySelector(".js-name-input")
+    emailInput = document.querySelector(".js-email-input")
+    line1Input = document.querySelector(".js-line1-input")
+    postcodeInput = document.querySelector(".js-postcode-input")
+    townInput = document.querySelector(".js-town-input")
+    countyInput = document.querySelector(".js-county-input")
+
+    nameInput.addEventListener("blur", () => {
+        if (validName(nameInput.value)) {
+            nameInput.classList.remove("is-invalid")
+        } else {
+            nameInput.classList.add("is-invalid")
         }
-        else {
-            name_input.classList.remove("is-invalid")
+    })
+
+    emailInput.addEventListener("blur", () => {
+        if (validEmail(emailInput.value)) {
+            emailInput.classList.remove("is-invalid")
+        } else {
+            emailInput.classList.add("is-invalid")
         }
+    })
 
-        if (email_input.value == "") {
-            email_input.classList.add("is-invalid")
-
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
-
+    postcodeInput.addEventListener("blur", () => {
+        if (validPostcode(postcodeInput.value)) {
+            postcodeInput.classList.remove("is-invalid")
+        } else {
+            postcodeInput.classList.add("is-invalid")
         }
-        else {
-            email_input.classList.remove("is-invalid")
+    })
+
+    line1Input.addEventListener("blur", () => {
+        if (line1Input.value != "") {
+            line1Input.classList.remove("is-invalid")
+        } else {
+            line1Input.classList.add("is-invalid")
         }
+    })
 
-
-        if (line1_input.value == "") {
-            line1_input.classList.add("is-invalid")
-
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
-
+    townInput.addEventListener("blur", () => {
+        if (townInput.value != "") {
+            townInput.classList.remove("is-invalid")
+        } else {
+            townInput.classList.add("is-invalid")
         }
-        else {
-            line1_input.classList.remove("is-invalid")
+    })
+
+    countyInput.addEventListener("blur", () => {
+        if (countyInput.value != "") {
+            countyInput.classList.remove("is-invalid")
+        } else {
+            countyInput.classList.add("is-invalid")
         }
-        if (postcode_input.value == "") {
-            postcode_input.classList.add("is-invalid")
+    })
+}
 
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
+function submitOrder() {
+    nameInput = document.querySelector(".js-name-input")
+    emailInput = document.querySelector(".js-email-input")
+    line1Input = document.querySelector(".js-line1-input")
+    postcodeInput = document.querySelector(".js-postcode-input")
+    townInput = document.querySelector(".js-town-input")
+    countyInput = document.querySelector(".js-county-input")
+    selectedPartitionInput = document.querySelector(".js-selected-partition")
 
-        }
-        else {
-            postcode_input.classList.remove("is-invalid")
-        }
-        if (town_input.value == "") {
-            town_input.classList.add("is-invalid")
-
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
-
-        }
-        else {
-            town_input.classList.remove("is-invalid")
-        }
-
-
-        if (county_input.value == "") {
-            county_input.classList.add("is-invalid")
-
-            submit_button.classList.add("is-invalid")
-            setTimeout(function () {
-                submit_button = document.querySelector(".js-form-submit")
-                submit_button.classList.remove("is-invalid")
-            }, 500)
-            return
-
-        }
-        else {
-            county_input.classList.remove("is-invalid")
-        }
-
+    if (validInputs()) {
         axios.request({
             method: "POST", url: "/api/submit",
             data: {
-                partition: selected_partition_input.value, name: name_input.value, email: email_input.value,
-                postcode: postcode_input.value, line1: line1_input.value, town: town_input.value, county: county_input.value
+                partition: selectedPartitionInput.value, name: nameInput.value, email: emailInput.value,
+                postcode: postcodeInput.value, line1: line1Input.value, town: townInput.value, county: countyInput.value
             }
         })
             .then(function (response) {
                 // handle success
-
-                address_page.classList.remove("is-shown")
-                completion_page.classList.add("is-shown")
+                addressPage.classList.remove("is-shown")
+                completionPage.classList.add("is-shown")
 
             })
             .catch(function (error) {
                 // handle error
-
-                submit_button.innerHTML = "Something went wrong!"
-                submit_button.value = "Something went wrong!"
-                submit_button.classList.add("is-invalid")
-                setTimeout(function () {
-                    submit_button = document.querySelector(".js-form-submit")
-                    submit_button.classList.remove("is-invalid")
-                }, 500)
+                submitButton.innerHTML = "Something went wrong!"
+                submitButton.value = "Something went wrong!"
+                submitButtonShake()
                 return
             })
+    } else {
+        return
+    }
+}
 
-    })
-})
+function validInputs() {
+    nameInput = document.querySelector(".js-name-input")
+    emailInput = document.querySelector(".js-email-input")
+    line1Input = document.querySelector(".js-line1-input")
+    postcodeInput = document.querySelector(".js-postcode-input")
+    townInput = document.querySelector(".js-town-input")
+    countyInput = document.querySelector(".js-county-input")
 
-function removeSubmitButtonClass() {
+    var allInputsValid = true
 
+    if (validName(nameInput.value)) {
+        nameInput.classList.remove("is-invalid")
+    } else {
+        nameInput.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
 
+    if (validEmail(emailInput.value)) {
+        emailInput.classList.remove("is-invalid")
+    } else {
+        emailInput.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
+
+    if (line1Input.value != "") {
+        line1Input.classList.remove("is-invalid")
+    } else {
+        line1Input.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
+
+    if (validPostcode(postcodeInput.value)) {
+        postcodeInput.classList.remove("is-invalid")
+    } else {
+        postcodeInput.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
+
+    if (townInput.value != "") {
+        townInput.classList.remove("is-invalid")
+    } else {
+        townInput.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
+
+    if (countyInput.value != "") {
+        countyInput.classList.remove("is-invalid")
+    } else {
+        countyInput.classList.add("is-invalid")
+        submitButtonShake()
+        allInputsValid = false
+    }
+    return allInputsValid
+}
+
+function validName(nameString) {
+    const atLeastTwoWords = (nameString.trim().indexOf(' ') != -1)
+    return atLeastTwoWords
+}
+
+function validEmail(emailString) {
+    var regex = RegExp('^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$')
+    var emailValid = regex.test(emailString.trim())
+    return emailValid
+}
+
+function validPostcode(postcodeString) {
+    var regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$')
+    var postcodeValid = regex.test(postcodeString.trim())
+    return postcodeValid
+}
+
+function submitButtonShake() {
+    submitButton.classList.add("is-invalid")
+    setTimeout(function () {
+        submitButton = document.querySelector(".js-form-submit")
+        submitButton.classList.remove("is-invalid")
+    }, 500)
 }
