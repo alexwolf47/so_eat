5e7a5078effa6699303d55989a6a45841419fcd8
alexwolf47 <alexwolf47@gmail.com>
2021-07-19 13:17:42 +0100

    chore: configure app to be deployable on Render

--- config/releases.exs
+++ config/releases.exs
@@ -2,7 +2,7 @@
 # from environment variables. You can also hardcode secrets,
 # although such is generally not recommended and you have to
 # remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
@@ -12,6 +12,7 @@ secret_key_base =
     """
 
 config :so_eat, SoEatWeb.Endpoint,
+  server: true,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
